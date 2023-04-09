45ff8e0f211bd1b9f8d1c572a4451b05feaad277
BOKER-THK <bokeritai@gmail.com>
2023-04-09 13:19:32 +0300

    replaced the naive approach with a flask database which can filter values using optional HTTP headers.

--- flaskdb.py
+++ flaskdb.py
@@ -1,50 +1,69 @@
+import os
 import sqlite3
-from dbutils import Database
-from flask import Flask, g
-from markupsafe import escape
+import click
+from flask import Flask, g, request
 
 app = Flask(__name__)
-DATA_PATH = ':memory:'
+DATA_PATH = os.path.join(app.instance_path, 'flask_db.sqlite')
+
+people = [('Abraham', '1', 'Be\'er Sheva'),
+          ('Isaac', '1', 'Be\'er Sheva'),
+          ('Jacob', '3', 'Shkhem'),
+          ('Joseph', '4', 'Goshen'),
+          ('Moses', '1', 'Sinai'),
+          ('Aharon', '6', 'Hor Hahar'),
+          ('David', '7', 'Jerusalem')]
+
+
+def db_to_str(db_res):
+    res = []
+    for item in db_res:
+        res.extend([{'name': item['name'], 'id': str(item['id']), 'address': item['address']}])
+    return res
 
 
 def get_db():
-    db = getattr(g, '_database', None)
+    db = getattr(g, 'database', None)
     if db is None:
-        db = g._database = sqlite3.connect(DATA_PATH)
+        db = g.database = sqlite3.connect(DATA_PATH)
     db.row_factory = sqlite3.Row
     return db
 
 
-def query_db(query, args=(), one=False):
-    cur = get_db().execute(query, args)
-    rv = cur.fetchall()
-    cur.close()
-    return (rv[0] if rv else None) if one else rv
-
-
 @app.teardown_appcontext
 def close_connection(exception):
-    db = getattr(g, '_database', None)
+    db = getattr(g, 'database', None)
     if db is not None:
         db.close()
 
 
-def insert(self, p: Database.Person):
-    query_db(''' INSERT INTO people VALUES (:name, :id, :address) ''',
-             {'name': p.name, 'id': p.id, 'address': p.address})
+@app.route("/")
+def select_elements():
+    name, id_, address = request.headers.get('name'),\
+                         request.headers.get('Id'),\
+                         request.headers.get('Address')
+    db: sqlite3.dbapi2 = get_db()
+
+    res = db.cursor().execute("SELECT * FROM people WHERE (:name IS NULL OR name = :name) AND\
+                                                          (:id IS NULL OR id = :id) AND\
+                                                          (:address IS NULL OR address = :address);",
+                              {'name': name, 'id': id_, 'address': address}).fetchall()
+
+    print(res)
+    return db_to_str(res)
 
 
-def update(self, id_: str, p: Database.Person):
-    query_db(''' UPDATE people SET address = :address, name = :name WHERE id = :id ''',
-             {'name': p.name, 'id': p.id, 'address': p.address})
+@click.command("init-db")
+def init_command():
+    db = get_db()
+    db.executescript('''
+    DROP TABLE IF EXISTS people;
+    CREATE TABLE people (name STRING, id INTEGER, address STRING);
+    ''')
+    for i in people:
+        db.execute("INSERT INTO people VALUES (?, ?, ?);", i)
+        db.commit()
+    click.echo("Database initialized.")
 
 
-def delete(self, id_: str):
-    query_db(''' DELETE FROM people WHERE id = :id ''',
-             {'id': id_})
-
-
-def present(self):
-    return query_db(''' SELECT * FROM people ''')
-
-
+app.cli.add_command(init_command)
--- main.py
+++ main.py
@@ -1,34 +1,6 @@
 import sqlite3
-from dbutils import Database
 
-conn = sqlite3.connect(':memory:')
+conn = sqlite3.connect("instance/flask_db.sqlite")
 
-people = [Database.Person('Abraham', '1', 'Be\'er Sheva'),
-          Database.Person('Isaac', '2', 'Grar'),
-          Database.Person('Jacob', '3', 'Shkhem'),
-          Database.Person('Joseph', '4', 'Goshen'),
-          Database.Person('Moses', '5', 'Sinai')]
-
-
-if __name__ == '__main__':
-    d = Database(conn)
-
-    inp = None
-    while inp != 'exit':
-        inp = input('enter command (1-insert, 2-update, 3-delete: ')
-        if inp == '1':
-            y = int(input('enter person number (1-5): '))
-            d.insert(people[y - 1])
-        elif inp == '2':
-            y = people[int(input('enter person number to update(1-5): ')) - 1].id
-            n_name = input('enter updated name: ')
-            n_add = input('enter updated address: ')
-            d.update(y, Database.Person(n_name, y, n_add))
-        elif inp == '3':
-            y = people[int(input('enter person number to delete (1-5): ')) - 1].id
-            d.delete(y)
-
-        conn.commit()
-        print(d.present())
-
-    conn.close()
+for i in conn.execute("SELECT * FROM people").fetchall():
+    print(i)
